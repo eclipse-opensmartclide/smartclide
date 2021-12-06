e8b179d2f1ddbc58db72df058b245e0889544f28
Philippe Krief <philippe.krief@eclipse-foundation.org>
2021-12-06 18:55:31 +0100

    Added Copy rights and comments + removed some absolute paths
    Signed-off-by: Philippe Krief <philippe.krief@eclipse-foundation.org>

--- IPCheck/dashAnalysis.sh
+++ IPCheck/dashAnalysis.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # ********************************************************************************
 # * Copyright (c) 2021, The Eclipse Foundation
@@ -14,42 +14,66 @@
 # ********************************************************************************
 #
 #--------------------------------------------------------------
-# This script runs the Eclipse DASH dependency analysis on a repository (Maven, Yarn or Nodes)
+# Runs the Eclipse DASH dependency analysis on a (Maven, Yarn or Nodes) repository 
+#		$1 	: To select between "Maven" | "Nodes" | "Yarn"
+#		$2	: Path of the folder containing the file to parse. The repo is supposed to be located under the $GIT_REPOS folder
+#		$3	: Optional - If the path of the folder is not the name of the repo then this third argument can specify the name of the repo
 #--------------------------------------------------------------
 
-MAVEN="/Users/philippe/Documents/Dev/apache-maven-3.6.3/bin/mvn"
+MAVEN="mvn"
 DASH_LIB="libs/org.eclipse.dash.licenses-0.0.1-SNAPSHOT.jar"
 
-GIT="/Users/philippe/git/"
-RES="./analysis/"
+if [[ -z GIT_REPOS ]]; then
+	echo "Please define the variable env. GIT_REPOS with the path to the folder containing all your GIT clones!"
+	exit
+fi 
+
+if [[ -z $1 ]]; then
+	echo "Syntax: dashAnalysis.sh <type> <repo-name> [<destination>]"
+	echo '  with:   <type>         ="Maven" or "Nodes" or "Yarn"'
+	echo '          <repo-name>    = Name of the git repo or path to the folder containing the pom.xml file'
+	echo '          <destination>  = If the path of the folder is not the name of the repo then this third argument specifies the name of the repo'
+	exit
+fi
+
+RES="./analysis"
 
 ECHO
 ECHO "+=====================================================+"
+ECHO "|                                                     |"
 ECHO "|                    DASH Analysis                    |"
+ECHO "|                                                     |"
 ECHO "+=====================================================+"
 ECHO
 
+# Grab arguments
 KIND_OF=$1 	# To select between "Maven" | "Nodes" | "Yarn"
-REPO=$2		# Path of the folder containing the file to parse. The repo is supposed to be located under the $GIT folder
+REPO=$2		# Path of the folder containing the file to parse. The repo is supposed to be located under the $GIT_REPOS folder
 DEST=$3		# If the path of the folder is not the name of the repo then this third argument can specify the name of the repo
 
-SRC_DIR=$GIT$REPO
+# Set source directory
+SRC_DIR=$GIT_REPOS"/"$REPO
 
-if [ -n $3 ]
-then
+# Set Destination directory
+if [[ -n $3 ]]; then
 	DEST=$3
 else
 	DEST=$REPO
 fi
-DEST_DIR=$RES$DEST
+DEST_DIR=$RES"/"$DEST
 
-if [ -n $1 ]; then
-	ECHO "+-----------------------------------------------------+"
-	ECHO ">> DASH analysis of: "$SRC_DIR", Type="$KIND_OF
-	ECHO ">> Results are available: "$DEST_DIR
-	ECHO "+-----------------------------------------------------+"
+# Start Analysis
+if [[ -n $1 ]]; then
+	ECHO ":"
+	ECHO ": DASH analysis of:......."$SRC_DIR
+	ECHO ": Type:..................."$KIND_OF
+	ECHO ": Results are available:.."$DEST_DIR
+	ECHO ":"
+	ECHO ":"
 	LOG_FILE=$DEST_DIR"/log.txt"
 	ANALYSIS_RESULT_FILE=$DEST_DIR"/03-"$DEST"-results.csv"
+	
+	# Create the destination Directory
 	MKDIR -p $DEST_DIR 
 	
 	if [ $KIND_OF = "Maven" ]; then
@@ -57,35 +81,38 @@ if [ -n $1 ]; then
 		DEP_LIST_FILE=$DEST_DIR"/01-"$DEST"-dependency_list.txt"
 		GREP_RESULT_FILE=$DEST_DIR"/02-grep.txt"
 		
-		ECHO ">> Starting analysis of:"$SRC_DIR
-		ECHO ">> mvn clean install > "$LOG_FILE
-		$MAVEN clean install -DskipTests -fae -f $SRC_DIR > $LOG_FILE
+		ECHO ": Starting analysis of:"$SRC_DIR
+		ECHO ": mvn clean install >> "$LOG_FILE
+		$MAVEN clean install -DskipTests -fae -f $SRC_DIR >> $LOG_FILE
 		
-		ECHO ">> mvn dependency:list > "$DEP_LIST_FILE
+		ECHO ": mvn dependency:list > "$DEP_LIST_FILE
 		$MAVEN dependency:list -f $SRC_DIR > $DEP_LIST_FILE
 		
-		ECHO ">> grep filter and sort > "$GREP_RESULT_FILE
+		ECHO ": grep filter and sort > "$GREP_RESULT_FILE
 		grep -ohE '\S+:(system|provided|compile)' $DEP_LIST_FILE | sort | uniq > $GREP_RESULT_FILE
 		
-		ECHO ">> DASH Analysis > " $ANALYSIS_RESULT_FILE
+		ECHO ": DASH Analysis > " $ANALYSIS_RESULT_FILE
 		java -jar $DASH_LIB $GREP_RESULT_FILE -summary $ANALYSIS_RESULT_FILE >> $LOG_FILE
+		
 	elif [ $KIND_OF = "Nodes" ]; then
 		# Analyse Nodes project
 		JSON_FILE=$SRC_DIR"/package-lock.json"
-		ECHO ">> Starting analysis of:"$JSON_FILE
+		ECHO ": Starting analysis of:"$JSON_FILE
 		
-		ECHO ">> DASH Analysis > " $ANALYSIS_RESULT_FILE
+		ECHO ": DASH Analysis > " $ANALYSIS_RESULT_FILE
 		java -jar $DASH_LIB $JSON_FILE -summary $ANALYSIS_RESULT_FILE >> $LOG_FILE
+		
 	elif [ $KIND_OF = "Yarn" ]; then
 		# Analyse Yarn project
 		YARN_FILE=$SRC_DIR"/yarn.lock"
-		ECHO ">> Starting analysis of:"$YARN_FILE
+		ECHO ": Starting analysis of:"$YARN_FILE
 		
-		ECHO ">> DASH Analysis > " $ANALYSIS_RESULT_FILE
+		ECHO ": DASH Analysis > " $ANALYSIS_RESULT_FILE
 		java -jar $DASH_LIB $YARN_FILE -summary $ANALYSIS_RESULT_FILE >> $LOG_FILE
 	fi
 	
-	ECHO ">> End of Analysis of:"$SRC_DIR
+	ECHO ":"
+	ECHO ": End of Analysis of:"$SRC_DIR
 fi
 
 
--- IPCheck/parseAll.sh
+++ IPCheck/parseAll.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # ********************************************************************************
 # * Copyright (c) 2021, The Eclipse Foundation
@@ -17,17 +17,17 @@
 # Runs a dependency analysis on each SmartCLIDE repo
 #--------------------------------------------------------------
 #
-#./dashAnalysis.sh "Maven" "smartclide-api-gateway"
-#./dashAnalysis.sh "Maven" "smartclide-broker"
-#./dashAnalysis.sh "Maven" "smartclide-cicd"
-#./dashAnalysis.sh "Maven" "smartclide-context"
+./dashAnalysis.sh "Maven" "smartclide-api-gateway"
+./dashAnalysis.sh "Maven" "smartclide-broker"
+./dashAnalysis.sh "Maven" "smartclide-cicd"
+./dashAnalysis.sh "Maven" "smartclide-context"
 ./dashAnalysis.sh "Maven" "smartclide-security/Theia-BackEnd" "smartclide-security"
-#./dashAnalysis.sh "Maven" "smartclide-service-creation"
-#./dashAnalysis.sh "Maven" "smartclide-TD-Interest"
-#./dashAnalysis.sh "Maven" "smartclide-TD-Reusability-Index"
-#./dashAnalysis.sh "Maven" "smartclide-TD-Principal"
-#./dashAnalysis.sh "Nodes" "smartclide-ide-front-end"
-#./dashAnalysis.sh "Nodes" "smartclide-perftestgen-theia"
-#./dashAnalysis.sh "Yarn" "smartclide-service-creation-theia"
-#./dashAnalysis.sh "Yarn" "smartclide-td-reusability-theia"
-#./dashAnalysis.sh "Yarn" "smartclide-design-pattern-selection-theia"
+./dashAnalysis.sh "Maven" "smartclide-service-creation"
+./dashAnalysis.sh "Maven" "smartclide-TD-Interest"
+./dashAnalysis.sh "Maven" "smartclide-TD-Reusability-Index"
+./dashAnalysis.sh "Maven" "smartclide-TD-Principal"
+./dashAnalysis.sh "Nodes" "smartclide-ide-front-end"
+./dashAnalysis.sh "Nodes" "smartclide-perftestgen-theia"
+./dashAnalysis.sh "Yarn" "smartclide-service-creation-theia"
+./dashAnalysis.sh "Yarn" "smartclide-td-reusability-theia"
+./dashAnalysis.sh "Yarn" "smartclide-design-pattern-selection-theia"
