e6dd43b614cc296d698cf44ba9799c234a6492dc
Philip Reimer <5737222+philipreimer@users.noreply.github.com>
2022-01-11 14:13:49 +0100

    add catch-all `else` to handle all unsupported project types
    update documentation

--- IPCheck/dashAnalysis.sh
+++ IPCheck/dashAnalysis.sh
@@ -15,10 +15,10 @@
 
 #
 #--------------------------------------------------------------
-# Runs the Eclipse DASH dependency analysis on a (Maven, Yarn or Nodes) repository 
+# Runs the Eclipse DASH dependency analysis on a (Maven, Yarn or Nodes) repository
 #		$1 	: To select between "Maven" | "Nodes" | "Yarn"
-#		$2	: The Path to the repository/folder where the config file is located
-#		$3	: Name to use (usually the name of the repo) 
+#		$2	: Path to the folder containing the config file
+#		$3	: Path of the folder where the results will be stored
 #--------------------------------------------------------------
 
 MAVEN="mvn"
@@ -28,7 +28,7 @@ DASH_LIB="$MAIN_DIR/libs/org.eclipse.dash.licenses-0.0.1-SNAPSHOT.jar"
 if [[ $# < 3 ]]; then
 	echo "ERROR: Wrong number of arguments"
 	echo "Syntax: dashAnalysis.sh <type> <source> <destination>"
-	echo '  with:   <type>         ="Maven" or "Nodes" or "Yarn"'
+	echo '  with:   <type>         = "Maven" or "Nodes" or "Yarn"'
 	echo '          <source>       = Path to the folder containing the config file'
 	echo '          <destination>  = Path of the folder where the results will be stored'
 	exit
@@ -54,13 +54,13 @@ echo ": Results are available in:.."$DEST_DIR
 echo ":"
 echo ":"
 # Check if the SRC_DIR directory does not exist
-if [ ! -d "$SRC_DIR" ] 
+if [ ! -d "$SRC_DIR" ]
 then
-    echo ": ERROR: Directory $SRC_DIR DOES NOT exists." 
+    echo ": ERROR: Directory $SRC_DIR DOES NOT exists."
     exit
 fi
 
-mkdir -p $DEST_DIR 
+mkdir -p $DEST_DIR
 
 LOG_FILE="$DEST_DIR/log.txt"
 ANALYSIS_RESULT_FILE="$DEST_DIR/03-dash_analysis.csv"
@@ -79,63 +79,62 @@ if [ $KIND_OF = "Maven" ]; then
 	DEP_LIST_FILE="$DEST_DIR/01-dependency_list.txt"
 	DEP_TREE_FILE="$DEST_DIR/01-dependency_tree.txt"
 	GREP_RESULT_FILE="$DEST_DIR/02-grep.txt"
-	
+
 	echo ": Starting analysis of: "$SRC_DIR
 	echo ": Starting analysis of: "$SRC_DIR >> $LOG_FILE
-	
+
 	echo ": mvn clean install >> "$LOG_FILE
 	echo ": mvn clean install >> "$LOG_FILE >> $LOG_FILE
 	$MAVEN clean install -DskipTests -fae -f $SRC_DIR >> $LOG_FILE
-	
+
 	echo ": mvn dependency:list > "$DEP_LIST_FILE
 	echo ": mvn dependency:list > "$DEP_LIST_FILE >> $LOG_FILE
 	$MAVEN dependency:list -f $SRC_DIR > $DEP_LIST_FILE
-	
+
 	echo ": mvn dependency:tree > "$DEP_TREE_FILE
 	echo ": mvn dependency:tree > "$DEP_TREE_FILE >> $LOG_FILE
 	$MAVEN dependency:tree -f $SRC_DIR > $DEP_TREE_FILE
-	
+
 	echo ": grep filter and sort > "$GREP_RESULT_FILE
 	echo ": grep filter and sort > "$GREP_RESULT_FILE >> $LOG_FILE
 	grep -ohE '\S+:(system|provided|compile)' $DEP_LIST_FILE | sort | uniq > $GREP_RESULT_FILE
-	
+
 	echo ": DASH Analysis > " $ANALYSIS_RESULT_FILE
 	echo ": DASH Analysis > " $ANALYSIS_RESULT_FILE >> $LOG_FILE
 	java -jar $DASH_LIB $GREP_RESULT_FILE -summary $ANALYSIS_RESULT_FILE >> $LOG_FILE
 	cp $SRC_DIR/pom.xml $DEST_DIR
-	
+
 elif [ $KIND_OF = "Nodes" ]; then
 	# Analyse Nodes project
 	JSON_FILE="$SRC_DIR/package-lock.json"
 	echo ": Starting analysis of: "$JSON_FILE
 	echo ": Starting analysis of: "$JSON_FILE >> $LOG_FILE
-	
+
 	echo ": DASH Analysis > " $ANALYSIS_RESULT_FILE
 	echo ": DASH Analysis > " $ANALYSIS_RESULT_FILE >> $LOG_FILE
 	java -jar $DASH_LIB $JSON_FILE -summary $ANALYSIS_RESULT_FILE >> $LOG_FILE
 	cp $SRC_DIR/package-lock.json $DEST_DIR
-	
+
 elif [ $KIND_OF = "Yarn" ]; then
 	# Analyse Yarn project
 	YARN_FILE="$SRC_DIR/yarn.lock"
 	echo ": Starting analysis of: "$YARN_FILE
 	echo ": Starting analysis of: "$YARN_FILE >> $LOG_FILE
-	
+
 	echo ": DASH Analysis > " $ANALYSIS_RESULT_FILE
 	echo ": DASH Analysis > " $ANALYSIS_RESULT_FILE >> $LOG_FILE
 	# Reduce the size of the batch (from 1000 to 500) to avoid a time out with ClearlyDefined
 	java -jar $DASH_LIB $YARN_FILE -summary $ANALYSIS_RESULT_FILE -batch 500 >> $LOG_FILE
 	cp $SRC_DIR/yarn.lock $DEST_DIR
-	
-elif [ $KIND_OF = "Docker" ]; then
-	# Analyse Docker project
-	
+
+else
+	# Unsupported project type
 	echo ": DASH Analysis > " $ANALYSIS_RESULT_FILE
 	echo ": DASH Analysis > " $ANALYSIS_RESULT_FILE >> $LOG_FILE
-	echo ">> Docker project are not analysed!"
+	echo ">> Unsupported project type \"${KIND_OF}\" cannot be analysed!"
+	echo ">> Unsupported project type \"${KIND_OF}\" cannot be analysed!" >> $LOG_FILE
 fi
 
 echo ":"
 echo ": End of Analysis of:"$SRC_DIR
 echo ": End of Analysis of:"$SRC_DIR >> $LOG_FILE
- 
